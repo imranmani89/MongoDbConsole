4e28649f1a9b8ee7d6dfba9e1d9265c4d375ec17
Imran <Imran@DESKTOP-KF34KBE>
2023-03-04 20:57:50 +0500

    Add project files.

---
+++ MongoDbConsole/PersonaModel.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbDemo;
+
+
+public class PersonModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId _id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public Degree[]? Degrees { get; set;}
+}
+
+public class Degree
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId _id { get; set; }
+    public string DegreeName { get; set; }
+}
